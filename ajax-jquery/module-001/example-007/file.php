791f1f90327bd39edad8e712f122370f352b4637
terry <terry.cadd@gmail.com>
2024-06-10 22:12:39 +0100

    following module 1 of jquery ajax

---
+++ ajax-jquery/module-001/example-007/file.php
@@ -0,0 +1,8 @@
+<?php
+/* echo 'hello this is from file.php'; */
+// see the GET request data
+echo 'see the get data';
+print_r($_GET);
+echo 'see the post data';
+print_r($_POST);
+?>
---
+++ ajax-jquery/module-001/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# copy all files across
+cp -rv /home/terry/code/web-development/ajax-jquery/module-001/* /var/www/html/ajax-jquery/module-001/
+
